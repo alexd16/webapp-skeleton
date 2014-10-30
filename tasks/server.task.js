635825d4b4704890aeac6abf5e2242e3fc017f05
Alexandre Gonçalves <a.goncalves@runtime-revolution.com>
2014-10-30 22:01:21 +0000

    Changed proxy implementation.

--- tasks/server.task.js
+++ tasks/server.task.js
@@ -1,18 +1,26 @@
+var baseDir = process.cwd();
+
 var gulp = require('gulp');
 var express = require('express');
 var connect = require('connect-livereload');
-var proxy = require('express-http-proxy');
 var lr = require('tiny-lr')();
+var request = require('request');
+
 
 module.exports = function(config, lr){
+  var stubs = require(config.proxy.stubs);
   return function() {
     var app = express();
     if (config.proxy) {
-      app.use('/api', proxy('localhost:3000', {
-        forwardPath: function(req, res) {
-          return require('url').parse(req.url).path;
+      app.use('/api', function(req, res){
+        if (stubs[req.path]) {
+          res.json(stubs[req.path]);
+        } else {
+          var path = 'http://localhost:3000'+req.url;
+          console.log(path);
+          req.pipe(request(path)).pipe(res);          
         }
-      }));
+      });
     }
     app.use(connect());
     app.use(express.static(config.root));
